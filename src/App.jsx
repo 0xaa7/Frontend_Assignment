188dbe9c092bd5f145cfb6f5136f0ebc0a118dae
Aamsher <hpandian008@gmail.com>
2024-01-26 00:38:13 +0530

    minor update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.scss";
-import Navbar from "./components/navbar/navbar";
+
 import styled from 'styled-components';
 import LandingPage from "./pages/landingPage/LandingPage";
 import Portfolio from "./components/portfolio/Portfolio";
 import WhyUsPage from "./pages/whyUsPage/WhyUsPage";
 import OurServices from "./pages/OurServices/OurServices";
 import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
 
 const StyledApp = styled.div`
   font-family: 'Quicksand', sans-serif;
